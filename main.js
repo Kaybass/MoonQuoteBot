e9cf7bb166200672fdf705c9846b5fe40b7dac35
Alexander Bean Apmann <caboose200@gmail.com>
2017-02-10 17:11:58 -0500

    fixed case where message is image with no text

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ var client = new Discord.Client();
 var express = require('express');
 var app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 /*
 load quotes here
@@ -33,7 +33,7 @@ var insertQuote = db.prepare("INSERT INTO quotes VALUES (?,?)");
 var quotes = new Array();
 
 //load quotes
-db.each("SELECT quote, author FROM quotes", function(err, row){
+db.each("SELECT quote, author FROM quotes", function (err, row) {
     quotes.push(new Array(row.quote, row.author))
 });
 
@@ -44,52 +44,56 @@ client.login(Settings.token);
 console.log(Responses.LOGGED_INTO_DISCORD);
 
 //event handler for message in a chat the bot is connected to or a DM
-client.on('message', function(message) {
+client.on('message', function (message) {
     var messageSplit = message.content.match(/("[^"]*")|[^ ]+/g);
 
-    //First we see if they gave the quote command with an option
-    if(messageSplit.length > 1 && messageSplit[0] == "/quote"){
-        
-        //Next we determine what option they used if they didn't fuck up
-        if(messageSplit[1] == "-a" || messageSplit[1] == "--add"){
+    //Case where message is an image with no text
+    if (messageSplit != null) {
 
-            if(messageSplit.length == 4 && messageSplit[2] != "" && messageSplit[3] != ""){
+        //First we see if they gave the quote command with an option
+        if (messageSplit.length > 1 && messageSplit[0] == "/quote") {
 
-                //Store to disk and memory
-                insertQuote.run(messageSplit[2].replace(/\"/g,""),messageSplit[3].replace(/\"/g,""));
-                quotes.push(new Array(messageSplit[2].replace(/\"/g,""),messageSplit[3].replace(/\"/g,"")))
+            //Next we determine what option they used if they didn't fuck up
+            if (messageSplit[1] == "-a" || messageSplit[1] == "--add") {
 
-                //Say that all is well
-                message.reply(Responses.QUOTE_ADDED);
-            
-            //John will eventually hit this
+                if (messageSplit.length == 4 && messageSplit[2] != "" && messageSplit[3] != "") {
+
+                    //Store to disk and memory
+                    insertQuote.run(messageSplit[2].replace(/\"/g, ""), messageSplit[3].replace(/\"/g, ""));
+                    quotes.push(new Array(messageSplit[2].replace(/\"/g, ""), messageSplit[3].replace(/\"/g, "")))
+
+                    //Say that all is well
+                    message.reply(Responses.QUOTE_ADDED);
+
+                    //John will eventually hit this
+                } else {
+                    message.reply(Responses.ADD_QUOTES_PROPER_USAGE)
+                }
+                //List the quotes, will in the future return a link to a webpage containing quotes
+            } else if (messageSplit[1] == "-l" || messageSplit[1] == "--list") {
+                var response = "\n";
+
+                quotes.forEach(function (quote) {
+                    response += '"' + quote[0] + '" -' + quote[1] + '\n';
+                });
+
+                message.reply(response);
+
+                //Help
+            } else if (messageSplit[1] == "-h" || messageSplit[1] == "--help") {
+                message.reply(Responses.HELP_MESSAGE);
+                //If they fucked up we give them help
             } else {
-                message.reply(Responses.ADD_QUOTES_PROPER_USAGE)
+                message.reply(Responses.HELP_MESSAGE);
             }
-        //List the quotes, will in the future return a link to a webpage containing quotes
-        } else if (messageSplit[1] == "-l" || messageSplit[1] == "--list") {
-            var response = "\n";
+            //For John
+        } else if (messageSplit.length > 0 && messageSplit.includes("@everyone")) {
+            message.reply(Responses.SOMEONE_SAID_ATEVERYONE);
 
-            quotes.forEach(function(quote){
-                response += '"' + quote[0] + '" -' + quote[1] + '\n';
-            });
-
-            message.reply(response);
-        
-        //Help
-        } else if (messageSplit[1] == "-h" || messageSplit[1] == "--help"){
-            message.reply(Responses.HELP_MESSAGE);
-        //If they fucked up we give them help
-        } else {
+            // if they just say /quote we also print the help message
+        } else if (messageSplit.length == 1 && messageSplit[0] == "/quote") {
             message.reply(Responses.HELP_MESSAGE);
         }
-    //For John
-    } else if (messageSplit.length > 0 && messageSplit.includes("@everyone")) {
-        message.reply(Responses.SOMEONE_SAID_ATEVERYONE);
-
-    // if they just say /quote we also print the help message
-    } else if (messageSplit.length == 1 && messageSplit[0] == "/quote"){
-        message.reply(Responses.HELP_MESSAGE);
     }
 
 });
@@ -106,12 +110,12 @@ app.get('/', function (req, res) {
 
 //This is just a plain old page-o
 app.get('/plainlist', function (req, res) {
-    
+
     res.render('plainlist', {
-        quotes : quotes
+        quotes: quotes
     });
 });
 
 app.listen(Settings.port, function () {
-    console.log('MoonBot web frontend listening on port',Settings.port,'!');
+    console.log('MoonBot web frontend listening on port', Settings.port, '!');
 });
